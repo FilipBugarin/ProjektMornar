bd70539c9d9ae73beadbc98886dd4e0432521b33
FilipBugarin <filip.bugarin@gmail.com>
2023-10-29 15:15:26 +0100

    response fix

--- src/main/java/hr/fer/security/JwtAuthenticationResponse.java
+++ src/main/java/hr/fer/security/JwtAuthenticationResponse.java
@@ -1,76 +1,19 @@
 package hr.fer.security;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 public class JwtAuthenticationResponse {
 	
 	private String accessToken;
 	private Long id;
-	private String role;
 	private String name;
 	private String email;
-	private String profileImageUrl;
-
-    public JwtAuthenticationResponse(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-	public JwtAuthenticationResponse(String accessToken, Long id, String role, String name, String email, String profileImageUrl) {
-		this.accessToken = accessToken;
-		this.id = id;
-		this.role = role;
-		this.name = name;
-		this.email = email;
-		this.profileImageUrl = profileImageUrl;
-	}
-
-	public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getProfileImageUrl() {
-		return profileImageUrl;
-	}
-
-	public void setProfileImageUrl(String profileImageUrl) {
-		this.profileImageUrl = profileImageUrl;
-	}
-
-	
-	
     
 }
--- src/main/java/hr/fer/security/controller/AuthController.java
+++ src/main/java/hr/fer/security/controller/AuthController.java
@@ -62,7 +62,7 @@ public class AuthController {
 
         User user = service.getUserById(tokenProvider.getUserIdFromJWT(jwt));
 
-        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt, user.getId(), null, user.getName(), user.getEmail(), null));
+        return ResponseEntity.ok(JwtAuthenticationResponse.builder().accessToken(jwt).id(user.getId()).name(user.getName()).email(user.getEmail()).build());
     }
 
     @SuppressWarnings({"unchecked", "rawtypes"})
@@ -135,7 +135,7 @@ public class AuthController {
 
         user = service.getUserById(tokenProvider.getUserIdFromJWT(jwt));
 
-        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt, user.getId(), null, user.getName(), user.getEmail(), null));
+        return ResponseEntity.ok(JwtAuthenticationResponse.builder().accessToken(jwt).id(user.getId()).name(user.getName()).email(user.getEmail()).build());
     }
 
 }
