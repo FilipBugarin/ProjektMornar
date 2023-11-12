b1c647ce987b8397255375218969a6b9ef45aea5
David Garić <david.garic@fer.hr>
2023-11-12 21:55:35 +0100

    Added quiz update

--- src/main/java/hr/fer/controller/QuizController.java
+++ src/main/java/hr/fer/controller/QuizController.java
@@ -3,6 +3,8 @@ package hr.fer.controller;
 import hr.fer.entity.Quiz;
 import hr.fer.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +29,18 @@ public class QuizController {
             return ResponseEntity.ok(true);
         } else {
             return ResponseEntity.ok(false);
+        }
+
+    }
+    
+    @PutMapping("update")
+    public ResponseEntity<Void> updateQuiz(@RequestBody Quiz quiz){
+    	HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Location", "/api/quiz/" + quiz.getId());
+        if(quizService.updateQuiz(quiz)){
+            return new ResponseEntity<>(headers, HttpStatus.CREATED);
+        } else {
+        	return new ResponseEntity<>(headers, HttpStatus.OK);
         }
 
     }
--- src/main/java/hr/fer/service/QuizService.java
+++ src/main/java/hr/fer/service/QuizService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -29,5 +30,40 @@ public class QuizService {
         }
         Quiz savedQuiz = quizRepository.save(quiz);
         return savedQuiz.getId() != null;
+    }
+    
+    /**
+     * 
+     * @param quiz
+     * @return true if a new instance was created, false otherwise
+     */
+    public boolean updateQuiz(Quiz quiz) {
+    	Optional<Quiz> optQuiz = quizRepository.findById(quiz.getId());
+    	//Save if the quiz was not created before
+    	if(!optQuiz.isPresent()) {
+    		createQuiz(quiz);
+    		return true;
+    	}
+    	
+    	//Update values if quiz exists
+    	Quiz existingQuiz = optQuiz.get();
+    	existingQuiz.setCreatedBy(quiz.getCreatedBy());
+    	existingQuiz.setDescription(quiz.getDescription());
+    	existingQuiz.setMasterQuiz(quiz.isMasterQuiz());
+    	existingQuiz.setPrivateQuiz(quiz.isPrivateQuiz());
+    	existingQuiz.setQuestionList(quiz.getQuestionList());
+    	for(Question q:quiz.getQuestionList()){
+            q.setQuiz(quiz);
+            for(Answer a:q.getAnswerList()){
+                a.setQuestion(q);
+            }
+        }
+    	existingQuiz.setQuizName(quiz.getQuizName());
+    	existingQuiz.setRandomOrder(quiz.isRandomOrder());
+    	existingQuiz.setTakenBy(quiz.getTakenBy());
+    	
+    	quizRepository.save(existingQuiz);
+    	
+        return false;
     }
 }
