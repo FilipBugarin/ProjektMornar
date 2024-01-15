0517f10f2971fde3664d99d74aa21658765ec73e
mstojak <mia.stojak29@gmail.com>
2024-01-15 17:09:19 +0100

    Changed cors to allow everyone everything

--- src/main/java/hr/fer/security/config/WebMvcConfig.java
+++ src/main/java/hr/fer/security/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://mornar.vrhunskaizvedba.hr")
+                .allowedOrigins("*")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SECS);
     }
